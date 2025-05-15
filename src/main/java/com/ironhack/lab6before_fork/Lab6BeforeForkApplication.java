f08a8e7e6dee9c59bd2339a6ef728cda3a37fa20
Marc Pedron Pladevall <marcpedron@192.168.1.3>
2025-05-15 20:32:34 +0200

    Initial commit with my Spring Boot project

---
+++ src/main/java/com/ironhack/lab6before_fork/Lab6BeforeForkApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.lab6before_fork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab6BeforeForkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab6BeforeForkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ironhack/lab6before_fork/Lab6BeforeForkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.lab6before_fork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab6BeforeForkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
